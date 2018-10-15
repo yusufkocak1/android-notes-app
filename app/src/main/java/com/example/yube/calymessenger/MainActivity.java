3409d36d3e4f5c562e99cdf6382194c096634a2a
yusufkocak1 <yusufkocak.53@yandex.com>
2018-10-15 16:52:37 +0300

    firebase data read

--- app/src/main/java/com/example/yube/calymessenger/MainActivity.java
+++ app/src/main/java/com/example/yube/calymessenger/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yube.calymessenger;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,14 +14,21 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.yube.calymessenger.Contact.Note;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Button exitBTN;
+
   //  private TextView testET;
 //public WebView mebis;
     @SuppressLint("WrongViewCast")
@@ -34,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
 
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("notes");
+
         exitBTN = findViewById(R.id.logoutBtn);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -46,12 +58,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.note_recyc);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.note_recyc);
 
-        ArrayList noteList  = new ArrayList<>();
-        noteAdapter adapter = new noteAdapter(this, noteList);
+        final ArrayList noteList  = new ArrayList<>();
+        final noteAdapter adapter = new noteAdapter(this, noteList);
 
-        noteList.add(new Note(1, "test", "test test test test test 123", "14-10-2018", "warning", 1));
+        
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds:dataSnapshot.getChildren()
+                     ) {
+                    noteList.add(new Note(1, ds.child("head").getValue(String.class), ds.child("content").getValue(String.class), ds.child("date").getValue(String.class), ds.child("type").getValue(String.class), 1));
+                adapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+         
 
 
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
